79339e021c707cd56bf88a3e3e99a5a6542c7000
Harsh Solanki <70328005+harshnsolanki@users.noreply.github.com>
2022-01-16 21:59:57 +0530

    Css Is Working Properly In Home Page.

--- cake4you/cake4you/settings.py
+++ cake4you/cake4you/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    'shop'
 ]
 
 MIDDLEWARE = [
@@ -118,4 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "shop/static",
+]
+
+# STATIC_ROOT = "/shop/static/"
