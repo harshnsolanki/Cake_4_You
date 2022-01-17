a6c7512efbaf878917b916f0d8433734571f3a4a
Harsh Solanki <70328005+harshnsolanki@users.noreply.github.com>
2022-01-17 12:30:52 +0530

    Basic Template File Created.

--- cake4you/shop/views.py
+++ cake4you/shop/views.py
@@ -1,6 +1,10 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request,'basic.html')
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')    
